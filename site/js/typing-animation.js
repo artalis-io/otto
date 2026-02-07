8ce916ce8432b457736cdc3cac8abb49657b27b0
Mark Farkas <mark@artalis.io>
2026-02-07 21:55:42 +0100

    Simplify cursor: solid during typing, no cursor at end
    - Remove blink during typing for better visibility
    - Restore original content at end instead of custom cursor
    - Add white-space: pre to cursor for consistent rendering

--- site/js/typing-animation.js
+++ site/js/typing-animation.js
@@ -118,36 +118,8 @@
                 block.classList.add('typed');
                 // Remove fixed height constraint now that content is complete
                 pre.style.minHeight = '';
-                // Final content with cursor
-                let finalHTML = '';
-                for (let i = 0; i < chars.length; i++) {
-                    const c = chars[i];
-                    const prevWrappers = i > 0 ? chars[i-1].wrappers : [];
-                    const currWrappers = c.wrappers;
-                    let commonLen = 0;
-                    while (commonLen < prevWrappers.length &&
-                           commonLen < currWrappers.length &&
-                           prevWrappers[commonLen].tag === currWrappers[commonLen].tag &&
-                           prevWrappers[commonLen].className === currWrappers[commonLen].className) {
-                        commonLen++;
-                    }
-                    for (let j = prevWrappers.length - 1; j >= commonLen; j--) {
-                        finalHTML += `</${prevWrappers[j].tag}>`;
-                    }
-                    for (let j = commonLen; j < currWrappers.length; j++) {
-                        const w = currWrappers[j];
-                        finalHTML += w.className ? `<${w.tag} class="${w.className}">` : `<${w.tag}>`;
-                    }
-                    finalHTML += escapeHTML(c.char);
-                }
-                if (chars.length > 0) {
-                    const lastWrappers = chars[chars.length - 1].wrappers;
-                    for (let j = lastWrappers.length - 1; j >= 0; j--) {
-                        finalHTML += `</${lastWrappers[j].tag}>`;
-                    }
-                }
-                finalHTML += '<span class="typed-cursor">\u2588</span>';
-                pre.innerHTML = finalHTML;
+                // Restore original content without cursor
+                pre.innerHTML = originalHTML;
                 return;
             }
 
